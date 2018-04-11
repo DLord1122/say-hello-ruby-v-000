de10df978f5d456a47e9042309668ae50d196048
Dennis Lord <lorddennis1006@gmail.com>
2018-04-11 18:58:13 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,4 @@
 # Build your say_hello method here
-def say_hello(name)
-  puts name="Gabriella"
+def say_hello(greeting name)
+  puts say_hello(greeting name)
+  greeting= "Hello", name="Gabriella!"
